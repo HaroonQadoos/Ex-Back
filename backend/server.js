d44e1cd8e4ddd86551261734d47302e8c9efa8bc
Haroon Qadoos <haroonriaz034@gmail.com>
2026-01-08 19:21:52 +0500

    updated test

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use("/api/upload", uploadRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
-api.use("/api", testRoutes);
+app.use("/api", testRoutes);
 
 const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
