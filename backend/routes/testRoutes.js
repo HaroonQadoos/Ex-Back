2ac8a06e2fff4cd532b35f68284dab259ba5c4e0
Haroon Qadoos <haroonriaz034@gmail.com>
2026-01-08 19:16:58 +0500

    updated 2

--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const cloudinary = require("../config/cloudinary");
+const { v2: cloudinary } = require("cloudinary");
 
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 router.get("/test-cloudinary", async (req, res) => {
   try {
     const result = await cloudinary.uploader.upload(
